403f1ced19606698e10e3173d4d49243e1b89caf
Cömert Çimen <comertcimen@gmail.com>
2025-03-12 12:48:10 +0100

    use generate client and extend

--- scripts/openapi-create-client.ts
+++ scripts/openapi-create-client.ts
@@ -1,7 +1,6 @@
 import { createClient, defaultPlugins } from "@hey-api/openapi-ts";
 
 createClient({
-  client: "@hey-api/client-fetch",
   input: "app_store_connect_openapi.json",
   output: {
     path: "src/client",
@@ -10,22 +9,13 @@ createClient({
   logs: {
     level: "error",
   },
-  experimentalParser: false,
   plugins: [
     ...defaultPlugins,
     "@hey-api/schemas",
-    {
-      dates: true,
-      name: "@hey-api/transformers",
-    },
+    "@hey-api/client-fetch",
     {
       enums: "javascript",
       name: "@hey-api/typescript",
-    },
-    {
-      name: "@hey-api/sdk",
-      auth: false,
-      transformer: true,
     },
   ],
 });
--- src/app-store-connect-api.ts
+++ src/app-store-connect-api.ts
@@ -1,75 +1,59 @@
-import { createClient } from '@hey-api/client-fetch'
-import { generateAuthToken, type AuthToken } from './auth'
+import { generateAuthToken, type AuthToken } from "./auth";
+import { client } from "./client/client.gen";
 
 const createAppStoreConnectApiClient = async ({
-  privateKeyId, 
-  issuerId, 
-  privateKey
+  privateKeyId,
+  issuerId,
+  privateKey,
 }: {
-  privateKeyId: string
-  issuerId: string
-  privateKey: string
+  privateKeyId: string;
+  issuerId: string;
+  privateKey: string;
 }) => {
-  
-  const api = await import('./client/sdk.gen')
-  let authToken: AuthToken | undefined = undefined
-  const client = createClient({
-    baseUrl: 'https://api.appstoreconnect.apple.com',
-    fetch: async (request) => {
-
-      if(!authToken || authToken.expireAt < Date.now()) {
-
-        authToken = await generateAuthToken({
-          privateKeyId: privateKeyId,
-          issuerId: issuerId,
-          privateKey: privateKey,
-        })
-      }
-
-      request.headers.set('Authorization', `Bearer ${authToken.token}`)
-
-      return fetch(request)
-    },
-    querySerializer: {
-      array: {
-        explode: false,
-        style: 'form'
-      }
+  const api = await import("./client/sdk.gen");
+  let authToken: AuthToken | undefined = undefined;
+  client.interceptors.request.use(async (request) => {
+    if (!authToken || authToken.expireAt < Date.now()) {
+      authToken = await generateAuthToken({
+        privateKeyId: privateKeyId,
+        issuerId: issuerId,
+        privateKey: privateKey,
+      });
     }
-  })
-  
+
+    request.headers.set("Authorization", `Bearer ${authToken.token}`);
+    return request;
+  });
+
   const apiWrapper = {
-    ...(api as Omit<typeof api, 'client'>)
-  }
-  delete (apiWrapper as any).client
+    ...(api as Omit<typeof api, "client">),
+  };
+  delete (apiWrapper as any).client;
 
   //wrap all functions, pass the client into options object, if not already present
-  for(const key in apiWrapper) {
-
+  for (const key in apiWrapper) {
     //forego the type checking, as it is very slow with hundreds of functions
     const value = (apiWrapper as any)[key];
-    
-    if(typeof value === 'function') {
-      (apiWrapper as any)[key] = async (...args: any[]) => {
 
-        if(args.length == 0) {
+    if (typeof value === "function") {
+      (apiWrapper as any)[key] = async (...args: any[]) => {
+        if (args.length == 0) {
           args.push({
             client: client,
-          })
-        }
-        else {
+          });
+        } else {
           args[0] = {
             client: client,
-            ...args[0]
-          }
+            ...args[0],
+          };
         }
 
-        return value(...args)
-      }
+        return value(...args);
+      };
     }
   }
 
   return apiWrapper;
-}
+};
 
-export default createAppStoreConnectApiClient
+export default createAppStoreConnectApiClient;
